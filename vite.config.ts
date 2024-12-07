bc998bec31d452df8086350cfc9b5fe61dcd0c2d
Ronan O'Leary <roleary81@gmail.com>
2024-12-07 12:41:03 +0100

    update vite config to

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,6 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	build: {
-		outDir: 'build', // Set your custom build directory here
-	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
