707fe49219829e2084b34c71fb812429295c1cf2
Ronan O'Leary <roleary81@gmail.com>
2024-12-07 12:36:53 +0100

    add adaptor file for vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,9 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-
+	build: {
+		outDir: 'build', // Set your custom build directory here
+	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
